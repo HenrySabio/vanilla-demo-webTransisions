ce19c65cfbef7cc79c1c6370a06653ce6d449995
Henry Sabio <henry@sabio.sh>
2025-09-17 16:03:45 -0400

    feat: added gsap transitions combined with barba.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,71 @@
-const headers = document.querySelectorAll('h2, h3');
-const imageWrappers = document.querySelectorAll('div.image');
+const transitionsInit = () => {
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio >= 0.1) {
-            console.log('in view', entry.target);
-            entry.target.classList.add('in-view')
-        } else {
-            console.log('out of view', entry.target);
-            entry.target.classList.remove('in-view')
-        }
+    const headers = document.querySelectorAll('h2, h3');
+    const imageWrappers = document.querySelectorAll('div.image');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio >= 0.1) {
+                console.log('in view', entry.target);
+                entry.target.classList.add('in-view')
+            } else {
+                console.log('out of view', entry.target);
+                entry.target.classList.remove('in-view')
+            }
+        });
+    }, {
+        threshold: [0, 0.1, 1]
     });
-}, {
-    threshold: [0, 0.1, 1]
-});
 
-headers.forEach(header => {
-    observer.observe(header);
-});
+    headers.forEach(header => {
+        observer.observe(header);
+    });
 
-imageWrappers.forEach(wrapper => {
-    observer.observe(wrapper);
+    imageWrappers.forEach(wrapper => {
+        observer.observe(wrapper);
+    });
+}
+
+transitionsInit();
+
+barba.init({
+    transitions: [{
+        name: 'switch',
+        leave({ current, next, trigger }) {
+            return new Promise(resolve => {
+                const timeline = gsap.timeline({
+                    onComplete() {
+                        current.container.remove()
+                        resolve()
+                    }
+                })
+
+                timeline
+                    .to("header", { y: "-100%" }, 0)
+                    .to("footer", { y: "100%" }, 0)
+                    .to(current.container, { opacity: 0 })
+            })
+        },
+        enter({ current, next, trigger }) {
+
+            return new Promise(resolve => {
+                window.scrollTo({ top: 0, behavior: 'smooth' })
+
+                const timeline = gsap.timeline({
+                    onComplete() {
+                        transitionsInit()
+                        resolve()
+                    }
+                });
+
+                timeline
+                    .set(next.container, { opacity: 0 })
+                    .to("header", { y: "0%" })
+                    .to("footer", { y: "0%" })
+                    .to(next.container, { opacity: 1 })
+            })
+        }
+    }],
+    views: [],
+    debug: false
 });
